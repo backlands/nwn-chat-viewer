75ce7900d71bd17784d428918bc7293a36ca3c10
Eric Klempnauer <me@eric-klempnauer.com>
2021-12-31 19:58:20 -0700

    feature: cleanup ux of export to pdf

--- src/components/Header/Badge/index.jsx
+++ src/components/Header/Badge/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
+
 import './styles.scss';
 
 const Badge = () => (
@@ -11,9 +11,7 @@ const Badge = () => (
     target="_blank"
     rel="noreferrer"
   >
-    <FontAwesomeIcon
-      icon={faGithub}
-    />
+    <FontAwesomeIcon icon={faGithub} />
     View on GitHub
   </a>
 );
